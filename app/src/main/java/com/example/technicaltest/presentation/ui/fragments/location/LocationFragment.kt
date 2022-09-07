f927942057e799720709c94f9ad26353b929ed57
jesus <jclementeh07@gmail.com>
2022-09-07 00:10:43 -0500

    Location Service running

--- app/src/main/java/com/example/technicaltest/presentation/ui/fragments/location/LocationFragment.kt
+++ app/src/main/java/com/example/technicaltest/presentation/ui/fragments/location/LocationFragment.kt
@@ -1,60 +1,112 @@
 package com.example.technicaltest.presentation.ui.fragments.location
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.viewModels
 import com.example.technicaltest.R
+import com.example.technicaltest.databinding.FragmentLocationBinding
+import com.example.technicaltest.services.LocationService
+import com.example.technicaltest.services.LocationService.Companion.ACTION_START_LOCATION_SERVICE
+import com.example.technicaltest.services.LocationService.Companion.TAG_SERVICE_LOCATION
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LocationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class LocationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    // Binding
+    private var _binding: FragmentLocationBinding? = null
+    private val binding get() = _binding!!
+
+    // ViewModel
+    private val viewModel: LocationViewModel by viewModels()
+
+    // Permissions Launcher
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
+    // Validate Permissions
+    private var hasLocationCoarse = false
+    private var hasLocationFine = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            it.forEach { (permission, granted) ->
+                when (permission) {
+                    Manifest.permission.ACCESS_COARSE_LOCATION -> hasLocationCoarse = granted
+                    Manifest.permission.ACCESS_FINE_LOCATION -> hasLocationFine = granted
+                }
+            }
+            if (!hasLocationCoarse || !hasLocationFine) {
+                showAlertDialog()
+            }
+            else {
+                broadcastReceiver()
+            }
         }
+
+        permissionLauncher.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        ))
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+        _binding = FragmentLocationBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LocationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LocationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        viewModel.getLocation()
+    }
+
+    private fun broadcastReceiver() {
+        var broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val latitude = intent?.getStringExtra("latitude")?.toDouble()
+                val longitude = intent?.getStringExtra("longitude")?.toDouble()
+                if (latitude != null && longitude != null) {
+                    Log.d("Location", "Latitude: $latitude, Longitude: $longitude")
                 }
             }
+        }
+        val intentFilter = IntentFilter("android.intent.action.NEW_LOCATION")
+        requireActivity().registerReceiver(broadcastReceiver, intentFilter)
+
+        if (!LocationService.isRunning) {
+            val intent = Intent(requireContext(), LocationService::class.java)
+            intent.putExtra(TAG_SERVICE_LOCATION, ACTION_START_LOCATION_SERVICE)
+            requireContext().startService(intent)
+        }
+    }
+
+    private fun showAlertDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Permisos denegados")
+        builder.setMessage("Tu necesitas dar permisos para acceder a la ubicación")
+        builder.setPositiveButton("Ok") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
     }
 }
---
+++ app/src/main/java/com/example/technicaltest/services/LocationService.kt
@@ -0,0 +1,150 @@
+package com.example.technicaltest.services
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.os.Build
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class LocationService : Service() {
+
+    companion object {
+        var isRunning = false
+        const val TAG_SERVICE_LOCATION = "LocationService"
+        const val NOTIFICATION_CHANNEL = "LocationServiceChannel"
+        const val ACTION_STOP_LOCATION_SERVICE = "ACTION_STOP_LOCATION_SERVICE"
+        const val ACTION_START_LOCATION_SERVICE = "ACTION_START_LOCATION_SERVICE"
+        private const val CODE_FOREGROUND_SERVICE = 1
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    @Singleton
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        when (intent?.getStringExtra(TAG_SERVICE_LOCATION)) {
+            ACTION_STOP_LOCATION_SERVICE -> {
+                onDestroy()
+            }
+        }
+
+        isRunning = true
+        startLocation(this)
+        showNotification()
+
+        return START_STICKY
+    }
+
+    private fun startLocation(context: Context) {
+
+        val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+
+        val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+
+        val locationManager = application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+        if (!hasAccessCoarseLocationPermission
+            || !hasAccessFineLocationPermission
+            || !isGpsEnabled) {
+            return@startLocation
+        } else {
+            lateinit var locationCallbackVar: LocationCallback
+
+            val locationRequestVar: LocationRequest = LocationRequest.create().apply {
+                interval = 300000
+                fastestInterval = 300000
+                priority = Priority.PRIORITY_HIGH_ACCURACY
+            }
+
+            locationCallbackVar = object : LocationCallback() {
+                override fun onLocationResult(p0: LocationResult) {
+                    super.onLocationResult(p0)
+                    Intent().also { intent ->
+                        intent.action = "android.intent.action.NEW_LOCATION"
+                        intent.putExtra("latitude", p0.lastLocation?.latitude.toString())
+                        intent.putExtra("longitude", p0.lastLocation?.longitude.toString())
+                        sendBroadcast(intent)
+                    }
+                }
+            }
+
+            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+            fusedLocationClient.requestLocationUpdates(
+                locationRequestVar,
+                locationCallbackVar,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+    private fun showNotification() {
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            try {
+                with(
+                    NotificationChannel(
+                        NOTIFICATION_CHANNEL,
+                        "Make it Easy",
+                        NotificationManager.IMPORTANCE_DEFAULT
+                    )
+                ) {
+                    enableLights(false)
+                    setShowBadge(false)
+                    enableVibration(false)
+                    setSound(null, null)
+                    lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+                    manager.createNotificationChannel(this)
+                }
+            } catch (e: Exception) {
+                Log.d("Error", "showNotification: ${e.localizedMessage}")
+            }
+        }
+
+        with(
+            NotificationCompat.Builder(this, NOTIFICATION_CHANNEL)
+        ) {
+            setTicker(null)
+            setContentTitle("En ruta")
+            setContentText("Rastreo de servicio")
+            setAutoCancel(false)
+            setOngoing(true)
+            setWhen(java.lang.System.currentTimeMillis())
+            startForeground(CODE_FOREGROUND_SERVICE, build())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isRunning = false
+        stopForeground(true)
+        stopSelf()
+    }
+
+}
